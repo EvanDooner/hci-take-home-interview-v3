1e12c4c41bb0dc4d1c08712286fa66742b55f9a7
Evan Dooner <evanjdooner@gmail.com>
2025-02-13 18:10:50 +0000

    Allow user to navigate to and from patient information page

--- PatientAdministrationSystem.App/src/App.tsx
+++ PatientAdministrationSystem.App/src/App.tsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import PatientSearch from './views/PatientSearch';
+import {findPatients, getPatient} from './services/patientSearchService';
+import Patient from './models/patient';
+import PaginatedResults from './models/paginatedResults';
+import PatientView from './views/PatientView';
+import PatientWithVisits from './models/patientWithVisits';
 
 enum Page {
   PatientSearch,
@@ -7,26 +13,38 @@ enum Page {
 }
 
 function App() {
-  const [currentPage] = useState(Page.PatientSearch);
+  const [currentPage, setCurrentPage] = useState(Page.PatientSearch);
+  const [currentPatient, setCurrentPatient] = useState<PatientWithVisits | undefined>(undefined);
+  const [patients, setPatients] = useState<PaginatedResults<Patient> | undefined>(undefined);
 
-  const page =  selectPage(currentPage);
+  const page =  selectPage();
+
+  function onSelect(patientId: string) {
+    getPatient(patientId, (patient) => {
+      setCurrentPatient(patient);
+      setCurrentPage(Page.PatientView);
+    });
+  }
+
+  function selectPage() {
+    switch(currentPage) {
+      case Page.PatientSearch:
+        return <PatientSearch patients={patients} onSearch={(searchQuery) => findPatients(searchQuery, setPatients)} onSelect={onSelect}/>
+      case Page.PatientView:
+        if (currentPatient === undefined) {
+          throw new Error("Cannot display patient view - no patient selected");
+        }
+        return <PatientView patient={currentPatient} onBack={() => setCurrentPage(Page.PatientSearch)}/>
+      default:
+        return <div>Default</div>
+    }
+  }
 
   return (
     <div id="app">
       {page}
     </div>
   )
-}
-
-function selectPage(page: Page) {
-  switch(page) {
-    case Page.PatientSearch:
-      return <div>PatientSearch</div>
-    case Page.PatientView:
-      return <div>PatientView</div>
-    default:
-      return <div>Default</div>
-  }
 }
 
 export default App
