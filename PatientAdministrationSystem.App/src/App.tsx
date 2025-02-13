d0eba1431c97927a527c2c48a2e26246b18047ce
Evan Dooner <evanjdooner@gmail.com>
2025-02-13 15:10:34 +0000

    Allow FE app to present different pages

--- PatientAdministrationSystem.App/src/App.tsx
+++ PatientAdministrationSystem.App/src/App.tsx
@@ -1,11 +1,32 @@
+import { useState } from 'react'
 import './App.css'
 
+enum Page {
+  PatientSearch,
+  PatientView
+}
+
 function App() {
+  const [currentPage] = useState(Page.PatientSearch);
+
+  const page =  selectPage(currentPage);
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>Implement me</h1>
+      {page}
     </div>
   )
+}
+
+function selectPage(page: Page) {
+  switch(page) {
+    case Page.PatientSearch:
+      return <div>PatientSearch</div>
+    case Page.PatientView:
+      return <div>PatientView</div>
+    default:
+      return <div>Default</div>
+  }
 }
 
 export default App
