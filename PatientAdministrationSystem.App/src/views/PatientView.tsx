69ecaf373835bbd0c233c25e083279347bbf6494
Evan Dooner <evanjdooner@gmail.com>
2025-02-13 18:41:08 +0000

    Restructure PatientView for easier styling

--- PatientAdministrationSystem.App/src/views/PatientView.tsx
+++ PatientAdministrationSystem.App/src/views/PatientView.tsx
@@ -1,19 +1,30 @@
 import PatientWithVisits from "../models/patientWithVisits";
 
+function formatDate(date: string) {
+    const parsedDate = new Date(date);
+
+    return `${parsedDate.toLocaleDateString()} at ${parsedDate.toLocaleTimeString()}`
+}
+
 export default function PatientView({patient, onBack}: {patient: PatientWithVisits, onBack: () => void}) {
     console.log(patient);
-    const visits = patient.visits.map(v => <li key={v.id}>{v.date}</li>)
+    const visits = patient.visits.map(v => <li key={v.id}>{formatDate(v.date)}</li>)
 
     return (
-        <div>
-            <button onClick={onBack}>Back</button>
+        <div className="patient-view">
+            <button className="back" onClick={onBack}>Back</button>
             <div data-patient-id={patient.id}>
-                <div>{patient.firstName}</div>
-                <div>{patient.lastName}</div>
-                <div>{patient.email}</div>
-                <ol>
-                    {visits}
-                </ol>
+                <div className="bio">
+                    <div className="bio-item"><span className="label">First Name:</span>{patient.firstName}</div>
+                    <div className="bio-item"><span className="label">Last Name:</span>{patient.lastName}</div>
+                    <div className="bio-item"><span className="label">Email:</span>{patient.email}</div>
+                </div>
+                <div className="visits">
+                    <div>Visits</div>
+                    <ol>
+                        {visits}
+                    </ol>
+                </div>
             </div>
         </div>
     );
