1a80ecc6add4e4e955acb4218068bc57b5dec6ac
Evan Dooner <evanjdooner@gmail.com>
2025-02-13 13:43:27 +0000

    Fetch PatientEntities using IPatientsRepository

--- PatientAdministrationSystem.Api/PatientAdministrationSystem.Application/Repositories/Interfaces/IPatientsRepository.cs
+++ PatientAdministrationSystem.Api/PatientAdministrationSystem.Application/Repositories/Interfaces/IPatientsRepository.cs
@@ -1,9 +1,46 @@
-using System.Linq.Expressions;
 using PatientAdministrationSystem.Application.Entities;
+using PatientAdministrationSystem.Application.Repositories.Pagination;
 
 namespace PatientAdministrationSystem.Application.Repositories.Interfaces;
 
 public interface IPatientsRepository
 {
     // Add interfaces here for your repository methods
+
+    /// <summary>
+    /// Gets a PatientEntity by ID, if one exists.
+    /// </summary>
+    /// <param name="id">The ID of the PatientEntity to return</param>
+    /// <param name="loadVisits">true if the returned PatientEntity should eagerly load related Visits; false otherwise</param>
+    /// <returns>the PatientEntity with the specified ID, if one exists</returns>
+    PatientEntity? GetPatient(Guid id, bool loadVisits);
+
+    /// <summary>
+    /// Returns the PatientEntities associated with the specified HospitalEntity ID that match the searchQuery.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Results are ordered by FirstName, LastName, Email, Id ascending.
+    /// </para>
+    /// <para>
+    /// All search query comparisons are done on lowercased values with spaces (' ') removed.
+    /// </para>
+    ///
+    /// A PatientEntity is considered to match the search query if any of the following are true:
+    ///
+    /// <list type="bullet">
+    ///     <item>
+    ///         <description>The combined FirstName and LastName contains the search query</description>
+    ///     </item>
+    ///     <item>
+    ///         <description>The Email contains the search query</description>
+    ///     </item>
+    /// </list>
+    /// </remarks>
+    /// <param name="hospitalId">The ID of an HospitalEntity. All matching PatientEntities must have a Visit associated with the HospitalIdentity</param>
+    /// <param name="searchQuery">The freetext string to match.</param>
+    /// <param name="pageNumber">The 1-indexed page to return from the full result set. Default 1. Minimum 1.</param>
+    /// <param name="pageSize">The maximum number of results to return. Default 10. Minimum 1.</param>
+    /// <returns>PatientEntities that match the search criteria</returns>
+    PaginatedResults<PatientEntity> FindPatients(Guid hospitalId, string searchQuery, int pageNumber = 1, int pageSize = 10);
 }
--- PatientAdministrationSystem.Api/PatientAdministrationSystem.Infrastructure/Repositories/PatientsRepository.cs
+++ PatientAdministrationSystem.Api/PatientAdministrationSystem.Infrastructure/Repositories/PatientsRepository.cs
@@ -1,7 +1,7 @@
-using System.Linq.Expressions;
-using Microsoft.EntityFrameworkCore;
+using System.Data.
 using PatientAdministrationSystem.Application.Entities;
 using PatientAdministrationSystem.Application.Repositories.Interfaces;
+using PatientAdministrationSystem.Application.Repositories.Pagination;
 
 namespace PatientAdministrationSystem.Infrastructure.Repositories;
 
@@ -17,4 +17,47 @@ public class PatientsRepository : IPatientsRepository
 
 
     // Add logic here for your querying the data context
+    public PaginatedResults<PatientEntity> FindPatients(Guid hospitalId, string searchQuery, int pageNumber = 1, int pageSize = 10)
+    {
+        if (pageNumber < 1)
+        {
+            throw new ArgumentException($"pageNumber should be greater than or equal to 1. Was: {pageNumber}");
+        }
+        if (pageSize < 1)
+        {
+            throw new ArgumentException($"pageSize should be greater than or equal to 1. Was: {pageSize}");
+        }
+
+        var allResults = _context.Patients.Where(p =>
+            p.PatientHospitals.Any(ph => ph.HospitalId == hospitalId) &&
+            (
+                (p.FirstName + p.LastName).Replace(" ", "").ToLower().Contains(searchQuery) ||
+                p.Email.Replace(" ", "").ToLower().Contains(searchQuery)
+            )
+        )
+        .OrderBy(p => p.FirstName)
+        .ThenBy(p => p.LastName)
+        .ThenBy(p => p.Email)
+        .ThenBy(p => p.Id);
+
+        var totalCount = allResults.Count();
+
+        var pageOfResults = allResults
+            .Skip(pageSize * (pageNumber - 1))
+            .Take(pageSize);
+
+        return new PaginatedResults<PatientEntity>(pageOfResults, pageNumber, pageSize, totalCount);
+    }
+
+    public PatientEntity? GetPatient(Guid id, bool loadVisits)
+    {
+        var patient = _context.Patients.Find(id);
+
+        if (patient != null && loadVisits)
+        {
+            _context.Entry(patient).Collection(p => p.PatientHospitals.Select(ph => ph.Visit)).Load();
+        }
+
+        return patient;
+    }
 }
