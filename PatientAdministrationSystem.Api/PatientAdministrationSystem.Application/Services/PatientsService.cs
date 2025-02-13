9f52312a3f30de6595fb5b8d20e00db258791ed9
Evan Dooner <evanjdooner@gmail.com>
2025-02-13 13:46:59 +0000

    Provide access to PatientEntities in IPatientService

--- PatientAdministrationSystem.Api/PatientAdministrationSystem.Application/Services/PatientsService.cs
+++ PatientAdministrationSystem.Api/PatientAdministrationSystem.Application/Services/PatientsService.cs
@@ -1,7 +1,7 @@
 using PatientAdministrationSystem.Application.Entities;
 using PatientAdministrationSystem.Application.Interfaces;
 using PatientAdministrationSystem.Application.Repositories.Interfaces;
-using System.Linq.Expressions;
+using PatientAdministrationSystem.Application.Repositories.Pagination;
 
 namespace PatientAdministrationSystem.Application.Services;
 
@@ -15,4 +15,23 @@ public class PatientsService : IPatientsService
     }
 
     // Define your patient search logic here based on the interface method definition
+    public PaginatedResults<PatientEntity> FindPatients(Guid hospitalId, string searchQuery, int pageNumber = 1, int pageSize = 10)
+    {
+        if (pageNumber < 1)
+        {
+            throw new ArgumentException($"pageNumber should be greater than or equal to 1. Was: {pageNumber}");
+        }
+        if (pageSize < 1)
+        {
+            throw new ArgumentException($"pageSize should be greater than or equal to 1. Was: {pageSize}");
+        }
+
+        return _repository.FindPatients(hospitalId, searchQuery, pageNumber, pageSize);
+    }
+
+    public PatientEntity? GetPatient(Guid id)
+    {
+        return _repository.GetPatient(id, true);
+    }
+
 }
