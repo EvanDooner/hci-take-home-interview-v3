2c8cca725d0c7275e1d8bf3ce4fe5b165a7abbd0
Evan Dooner <evanjdooner@gmail.com>
2025-02-13 18:09:48 +0000

    Use backend test API URL

--- PatientAdministrationSystem.App/src/api/apiClient.ts
+++ PatientAdministrationSystem.App/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: 'https://XXXXXX', // Replace with your API's base URL
+  baseURL: 'http://localhost:5272', // Replace with your API's base URL
   headers: {
     'Content-Type': 'application/json',
   },
